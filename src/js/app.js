2fcfc0b7a1f5b34cfefa45ac5e0a36117f1d01f9
Evgenii Nikiforov <Evgeniy.Nikiforov@rdtex.ru>
2024-09-11 00:26:56 +0300

    add firebase

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,28 @@ import '../css/app.scss';
 // Import App Component
 import Root from '../components/Root.jsx';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6XK-31SL7-YOxHbYV3qaZteYxKbU1Nzc",
+    authDomain: "radio-radonezh.firebaseapp.com",
+    projectId: "radio-radonezh",
+    storageBucket: "radio-radonezh.appspot.com",
+    messagingSenderId: "856115679491",
+    appId: "1:856115679491:web:bdfe64ea02cd8b40079936",
+    measurementId: "G-B9ZCF0654V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Init F7 React Plugin
 Framework7.use(Framework7React)
 
