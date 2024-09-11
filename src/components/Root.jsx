5f3705a24f5fad1a4626f434750906836ca7d35d
Evgenii Nikiforov <e@nikiforov.org>
2024-09-12 02:01:39 +0300

    Set browser autodetect theme

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -20,7 +20,7 @@ const RadonezhApp = () => {
     name: 'Радонеж', // App name
     theme: 'auto', // Automatic theme detection
 
-    //darkMode: true,
+    darkMode: window.matchMedia('(prefers-color-scheme: dark)'),
 
     // App store
     store: store,
